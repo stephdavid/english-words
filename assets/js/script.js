42cb9c2485a3c8218554e7b79767b92686a8d5cb
Stephanie David <stephdaviduk@gmail.com>
2023-12-27 00:41:02 +0000

    re-ordered the functions for a more logical and facter presentation of the definition and translations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,5 @@
 $(document).ready(function () {
 
-   function populateDefinition(data) {
-      $("#selectedWord").text(data.entry);
-      $("#noun").text(data.meaning.noun);
-      $("#verb").text(data.meaning.verb);
-      $("#adjective").text(data.meaning.adjective);
-      $("#adverb").text(data.meaning.adverb);
-
-   };
-
-   function getDefinition(selectedWord) {
-      const url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=" + selectedWord + "";
-      const options = {
-         method: "GET",
-         headers: {
-            "X-RapidAPI-Key": "edffdf1e95msha5ea98f7066bb52p15b514jsnb04cc7a266bf",
-            "X-RapidAPI-Host": "twinword-word-graph-dictionary.p.rapidapi.com"
-         }
-      };
-      fetch(url, options)
-         .then(function (response) {
-            if (!response.ok) {
-               throw new Error("Network response was not ok");
-            }
-            return response.json();
-         })
-         .then(function (data) {
-            console.log("API Response:", data);
-            populateDefinition(data, selectedWord);
-         })
-         .catch(function (error) {
-            console.error("Error fetching data:", error);
-            $("#stephModal-2").modal("show");
-         });
-   };
-
    function getGreekTranslation(selectedWord) {
       const url = "https://nlp-translation.p.rapidapi.com/v1/translate?text=" + selectedWord + "&to=el&from=en";
       const options = {
@@ -54,7 +19,6 @@ $(document).ready(function () {
          .then(function (dataT) {
             console.log("API Response:", dataT);
             $("#translation-el").text(dataT.translated_text.el);
-            getDefinition(selectedWord);
          })
          .catch(function (error) {
             console.error("Error fetching data:", error);
@@ -143,6 +107,42 @@ $(document).ready(function () {
          });
    };
 
+   function populateDefinition(data, selectedWord) {
+      $("#selectedWord").text(data.entry);
+      $("#noun").text(data.meaning.noun);
+      $("#verb").text(data.meaning.verb);
+      $("#adjective").text(data.meaning.adjective);
+      $("#adverb").text(data.meaning.adverb);
+      
+      getFrenchTranslation(selectedWord);
+   };
+
+   function getDefinition(selectedWord) {
+      const url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=" + selectedWord + "";
+      const options = {
+         method: "GET",
+         headers: {
+            "X-RapidAPI-Key": "edffdf1e95msha5ea98f7066bb52p15b514jsnb04cc7a266bf",
+            "X-RapidAPI-Host": "twinword-word-graph-dictionary.p.rapidapi.com"
+         }
+      };
+      fetch(url, options)
+         .then(function (response) {
+            if (!response.ok) {
+               throw new Error("Network response was not ok");
+            }
+            return response.json();
+         })
+         .then(function (data) {
+            console.log("API Response:", data);
+            populateDefinition(data, selectedWord);
+         })
+         .catch(function (error) {
+            console.error("Error fetching data:", error);
+            $("#stephModal-2").modal("show");
+         });
+   };
+
    function getSearchHistory() {
       // Get a string from local storage
       let getHistory = localStorage.getItem("selectedWord");
@@ -192,7 +192,7 @@ $(document).ready(function () {
          // Store the updated array back in local storage
          localStorage.setItem("selectedWord", JSON.stringify(existingHistory));
 
-         getFrenchTranslation(selectedWord);
+         getDefinition(selectedWord);
       }
 
       // Handle input event for typing only (a) space(s) and Enter
